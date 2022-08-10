0efb0b0c5252210f2782267e828cf09b16edcc03
jorged-zaz <gwero_11@hotmail.con>
2022-08-09 19:34:05 -0500

    primer commit

---
+++ curso_dart_hola_mundo_1/bin/curso_dart_hola_mundo_1.dart
@@ -0,0 +1,5 @@
+import 'package:curso_dart_hola_mundo_1/curso_dart_hola_mundo_1.dart' as curso_dart_hola_mundo_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curso_dart_hola_mundo_1.calculate()}!');
+}
---
+++ curso_dart_hola_mundo_1/test/curso_dart_hola_mundo_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_dart_hola_mundo_1/curso_dart_hola_mundo_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
